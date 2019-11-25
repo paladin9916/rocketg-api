612b6ff98da89d93a18323f3e280dacb3b4d63be
fulldev326 <vdzhandzhugazov@bk.ru>
2019-11-26 05:07:44 +1000

    cicd default db - integration

--- RocketG_api/settings.py
+++ RocketG_api/settings.py
@@ -80,10 +80,10 @@ WSGI_APPLICATION = 'RocketG_api.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # },
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
     'integration': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'py_rocketg_integration',
