120bb239db605bb16fe67c1288270d06be37582c
roman2055 <roman.b2055@gmail.com>
2019-11-27 14:08:25 +0000

    Update settings.py

--- RocketG_api/settings.py
+++ RocketG_api/settings.py
@@ -80,11 +80,11 @@ WSGI_APPLICATION = 'RocketG_api.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #},
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'integration': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'py_rocketg_integration',
         'USER': 'rocketg_db_1',
