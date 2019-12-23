7a145a6be242647a48a7952571f63425d3178a53
roman2055 <roman.b2055@gmail.com>
2019-12-24 05:41:34 +1000

    Don't agree duplicate phone number

--- apis/api_view/user.py
+++ apis/api_view/user.py
@@ -73,38 +73,42 @@ def userGetSave(request):
         paymentsCurrency = request.data.get('payments_currency')
 
         try:
-
             if Users.objects.get(email=email):
-                return Response(data={'success': False, 'error': ['Exist user. Please try again.']},
+                return Response(data={'success': False, 'error': ['Exist this email. Please try again.']},
                                 status=status.HTTP_200_OK)
         except Users.DoesNotExist:
-            password = get_random_string(length=16)
-            salt = settings.SECRET_KEY
-            encryptedPassword = make_password(password, salt=salt)
+            try:
+                if Users.objects.get(phone=phone):
+                    return Response(data={'success': False, 'error': ['Exist this phone number. Please try again.']},
+                                    status=status.HTTP_200_OK)
+            except Users.DoesNotExist:
+                password = get_random_string(length=16)
+                salt = settings.SECRET_KEY
+                encryptedPassword = make_password(password, salt=salt)
 
-            user = Users(email=email, firstname=firstname, lastname=lastname, encrypted_password=encryptedPassword, reset_password_token=password, phone=phone, job_title=jobTitle,
-                         department=department, language=language, role_id=roleId, company_id=companyId,
-                         reimbursement_cycle=reimbursementCycle, payments_currency=paymentsCurrency)
+                user = Users(email=email, firstname=firstname, lastname=lastname, encrypted_password=encryptedPassword, reset_password_token=password, phone=phone, job_title=jobTitle,
+                             department=department, language=language, role_id=roleId, company_id=companyId,
+                             reimbursement_cycle=reimbursementCycle, payments_currency=paymentsCurrency)
 
-        try:
-            user.save()
-            company = Companies.objects.get(id=companyId)
-            company.active_employees += 1
-            company.save()
-        except Users.DoesNotExist:
-            return Response(data={'success': False, 'error': ['Error in creating User.']}, status=status.HTTP_200_OK)
+                try:
+                    user.save()
+                    company = Companies.objects.get(id=companyId)
+                    company.active_employees += 1
+                    company.save()
+                except Users.DoesNotExist:
+                    return Response(data={'success': False, 'error': ['Error in creating User.']}, status=status.HTTP_200_OK)
 
-        # upload avatar
-        imageSerData = {
-            "user_id": user.id,
-            "avatar": avatar
-        }
-        imageSerializer = uploadImage(user.id, imageSerData)
-        user.avatar = imageSerializer.data.get('avatar')
-        user.save()
+                # upload avatar
+                imageSerData = {
+                    "user_id": user.id,
+                    "avatar": avatar
+                }
+                imageSerializer = uploadImage(user.id, imageSerData)
+                user.avatar = imageSerializer.data.get('avatar')
+                user.save()
 
-        userData = getUserDataWithPW([user, ])
-        return Response(data={'success': True, 'data': userData}, status=status.HTTP_200_OK)
+                userData = getUserDataWithPW([user, ])
+                return Response(data={'success': True, 'data': userData}, status=status.HTTP_200_OK)
 
 
 @api_view(['GET', 'PUT'])
@@ -117,7 +121,7 @@ def userDetailUpdate(request, pk):
     try:
         user = Users.objects.get(pk=pk)
     except Users.DoesNotExist:
-        return Response(data={'success': False, 'error': ['Company do not exist.']},
+        return Response(data={'success': False, 'error': ['User do not exist.']},
                         status=status.HTTP_200_OK)
 
     if request.method == 'GET':
@@ -138,33 +142,44 @@ def userDetailUpdate(request, pk):
         reimbursementCycle = request.data.get('reimbursement_cycle')
         paymentsCurrency = request.data.get('payments_currency')
 
-        user.email = email
-        user.firstname = firstname
-        user.lastname = lastname
-        user.phone = phone
-        user.job_title = jobTitle
-        user.department = department
-        user.language = language
-        user.role_id = roleId
-        user.company_id = companyId
-        user.reimbursement_cycle = reimbursementCycle
-        user.payments_currency = paymentsCurrency
-
-        # upload avatar
-        imageSerData = {
-            "user_id": pk,
-            "avatar": avatar
-        }
-        imageSerializer = uploadImage(pk, imageSerData)
-        user.avatar = imageSerializer.data.get('avatar')
-
         try:
-            user.save()
+            if Users.objects.get(email=email):
+                return Response(data={'success': False, 'error': ['Exist this email. Please try again.']},
+                                status=status.HTTP_200_OK)
         except Users.DoesNotExist:
-            return Response(data={'success': False, 'error': ['Error in updating user.']}, status=status.HTTP_200_OK)
+            try:
+                if Users.objects.get(phone=phone):
+                    return Response(data={'success': False, 'error': ['Exist this phone number. Please try again.']},
+                                    status=status.HTTP_200_OK)
+            except Users.DoesNotExist:
+                user.email = email
+                user.firstname = firstname
+                user.lastname = lastname
+                user.phone = phone
+                user.job_title = jobTitle
+                user.department = department
+                user.language = language
+                user.role_id = roleId
+                user.company_id = companyId
+                user.reimbursement_cycle = reimbursementCycle
+                user.payments_currency = paymentsCurrency
 
-        userData = getUserData([user, ])
-        return Response(data={'success': True, 'data': userData}, status=status.HTTP_200_OK)
+                # upload avatar
+                imageSerData = {
+                    "user_id": pk,
+                    "avatar": avatar
+                }
+                imageSerializer = uploadImage(pk, imageSerData)
+                user.avatar = imageSerializer.data.get('avatar')
+
+                try:
+                    user.save()
+                except Users.DoesNotExist:
+                    return Response(data={'success': False, 'error': ['Error in updating user.']},
+                                    status=status.HTTP_200_OK)
+
+                userData = getUserData([user, ])
+                return Response(data={'success': True, 'data': userData}, status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
