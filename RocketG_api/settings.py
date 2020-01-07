87e04a68d27c3a43b20ee468dc65f23f2abd3b20
roman2055 <roman.b2055@gmail.com>
2020-01-07 01:54:25 -0800

    configuration updated

--- RocketG_api/settings.py
+++ RocketG_api/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
     #    'ENGINE': 'django.db.backends.sqlite3',
     #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     #},
-    'integration': {
+    'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'py_rocketg_integration',
         'USER': 'rocketg_db_1',
@@ -97,7 +97,7 @@ DATABASES = {
         'HOST': '47.103.37.13',
         'PORT': '3306'
     },
-    'default': {
+    'staging': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'py_rocketg_staging',
         'USER': 'rocketg_db_1',
